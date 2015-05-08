7c4dbc4e2bebab385418b6dd4d9dcba7d75679b2
Persianscript <Persianscript@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-08 11:43:09 +0000

    version 2.3.8
    git-svn-id: https://plugins.svn.wordpress.org/persian-woocommerce/tags/2.3.8@1156049 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/rial-function.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+add_filter( 'woocommerce_currencies', 'add_my_currency' );
+
+ 
+
+function add_my_currency( $currencies ) {
+
+$currencies['IRR'] = __( 'ریال', 'woocommerce' );
+$currencies['IRT'] = __( 'تومان', 'woocommerce' );
+$currencies['IRHT'] = __( 'هزار تومان', 'woocommerce' );
+
+return $currencies;
+
+}
+
+add_filter('woocommerce_currency_symbol', 'add_my_currency_symbol', 10, 2);
+
+ 
+
+function add_my_currency_symbol( $currency_symbol, $currency ) {
+
+switch( $currency ) {
+
+case 'IRR': $currency_symbol = 'ریال'; break;
+case 'IRT': $currency_symbol = 'تومان'; break;
+case 'IRHT': $currency_symbol = 'هزار تومان'; break;
+
+}
+
+return $currency_symbol;
+
+}
+
+
+?>
